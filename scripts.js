ad8856d9fc3c01730c42f1e3ac526f63f8c2edd1
AllanGame <61366528+AllanGame@users.noreply.github.com>
2020-12-14 14:56:19 -0500

    test

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
+const song = document.getElementById('song')
 var i = 0;
 function change() {
-  var doc = document.body
-  var color = ["blue", "yellow", "green", "red"];
-  doc.style.backgroundColor = color[i];
+  let color = ["blue", "yellow", "green", "red"];
+  document.body.style.backgroundColor = color[i];
   i = (i + 1) % color.length;
 }
 setInterval(change, 300);
 
+function toggleSong() {
+
+    if(song.paused) {
+        song.play()
+        document.getElementById("icon").className = "fas fa-pause";
+    } else {
+        song.pause()
+        document.getElementById("icon").className = "fas fa-play";
+    }
+    
+}
